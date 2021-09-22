84147105ab1fa7cf94602845c9529406ef7c115b
jnntnym <jnntnym@gmail.com>
2021-09-21 23:08:38 -0400

    Git testing

---
+++ GitTest/gitTesting.java
@@ -0,0 +1,10 @@
+package GitTest;
+
+public class gitTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is git testing");
+	}
+
+}
